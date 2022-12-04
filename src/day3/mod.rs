25a9ae0bcffa29b614279b02cf4f3f778e6e18ca
Ignacio Vargas <ignaevc@gmail.com>
2022-12-04 15:51:56 -0600

    Day 3 part 1

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,5 +1,30 @@
+pub fn find_repeat(mut iter1: impl Iterator<Item=char>, iter2: impl Clone + Iterator<Item=char>) -> char {
+    iter1.find(|&c1| {
+        iter2.clone().find(|&c2| {
+            c1 == c2
+        }).is_some()
+    }).unwrap()
+}
+
+pub fn get_priority(c: char) -> i32{
+    if c.is_ascii_lowercase() {
+        return (c as i32) - 96
+    }
+    else if c.is_ascii_uppercase() {
+        return (c as i32) - 38
+    }
+    else {
+        panic!("Unexpected char passed to get_priority: '{}'", c);
+    }
+}
+
 pub fn task1(lines: impl Iterator<Item = String>) -> i32 {
-    0
+    lines.fold(0, |acc, line| {
+        let length = line.chars().count();
+        let repeat_char = find_repeat(line.chars().take(length/2), line.chars().skip(length/2));
+        //println!("Repeat char found in line {}: '{}'\tValue:{}",line,repeat_char, get_priority(repeat_char));
+        acc + get_priority(repeat_char)
+    })
 }
 
 pub fn task2(lines: impl Iterator<Item = String>) -> i32 {

da0a936152a69fbac3e914fc726492ca92d33422
Ignacio Vargas <ignaevc@gmail.com>
2022-12-05 16:08:17 -0600

    Completed day 5 :)

---
+++ src/day5/mod.rs
@@ -0,0 +1,86 @@
+fn init_crates(lines: &mut impl Iterator<Item = String>) -> Vec<Vec<char>> {
+    let mut lines_peek = lines.peekable();
+    let length = (lines_peek.peek().unwrap().chars().count() + 1) / 4;
+    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); length];
+
+    for line in lines_peek {
+        if !line.trim().starts_with('[') {
+            break;
+        }
+
+        let mut block_iter = line.chars();
+        for stack in &mut stacks {
+            let block: char = match block_iter.next_chunk::<4>() {
+                Ok(val) => val[1],
+                Err(mut val) => val.nth(1).unwrap(),
+            };
+            if !block.is_whitespace() {
+                stack.insert(0, block);
+            }
+        }
+    }
+
+    // Advance lines past blank line
+    lines.next();
+
+    stacks
+}
+
+pub fn task1(mut lines: impl Iterator<Item = String>) -> String {
+    let mut stacks: Vec<Vec<char>> = init_crates(&mut lines);
+
+    for line in lines {
+        let instructions: Vec<usize> = line
+            .split_ascii_whitespace()
+            .filter_map(|s| s.parse().ok())
+            .collect();
+
+        let ammt = instructions[0];
+        let from = instructions[1] - 1;
+        let to = instructions[2] - 1;
+        for _ in 0..ammt {
+            let c = stacks[from].pop().unwrap();
+            stacks[to].push(c);
+        }
+    }
+
+    stacks
+        .into_iter()
+        .map(|stack| stack.last().unwrap().clone())
+        .collect()
+}
+
+pub fn task2(mut lines: impl Iterator<Item = String>) -> String {
+    let mut stacks: Vec<Vec<char>> = init_crates(&mut lines);
+    println!("Current stacks: {:?}", stacks);
+
+    for line in lines {
+        let instructions: Vec<usize> = line
+            .split_ascii_whitespace()
+            .filter_map(|s| s.parse().ok())
+            .collect();
+
+        let ammt = instructions[0];
+        let (from, to) = if instructions[1] < instructions[2] {
+            let (first_half, second_half) = stacks.split_at_mut(instructions[2] - 1);
+            (
+                first_half.get_mut(instructions[1] - 1).unwrap(),
+                second_half.get_mut(0).unwrap(),
+            )
+        } else {
+            let (first_half, second_half) = stacks.split_at_mut(instructions[1] - 1);
+            (
+                second_half.get_mut(0).unwrap(),
+                first_half.get_mut(instructions[2] - 1).unwrap(),
+            )
+        };
+
+        let moved_blocks = from.drain(from.len() - ammt..from.len());
+        to.extend_from_slice(moved_blocks.as_slice());
+    }
+
+    stacks
+        .into_iter()
+        .map(|stack| stack.last().unwrap().clone())
+        .collect()
+}

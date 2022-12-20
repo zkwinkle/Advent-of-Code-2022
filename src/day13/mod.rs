8c7c3d284d07bf8c5bb1d0a29d90bb3c272081fe
Ignacio Vargas <ignaevc@gmail.com>
2022-12-20 16:10:56 -0600

    Day13 Part1

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,11 +1,33 @@
-use std::{fmt, mem, str::FromStr, string::ParseError};
+use std::{cmp::Ordering, fmt, str::FromStr, string::ParseError};
 
 use aoc_lib::tooling::SolutionResult;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum PacketData {
     List(Vec<PacketData>),
     Integer(u32),
+}
+
+impl PartialOrd for PacketData {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for PacketData {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (PacketData::List(l), i @ PacketData::Integer(_)) => {
+                l.cmp(&[i.clone()].into())
+            }
+            (i @ PacketData::Integer(_), PacketData::List(l)) => {
+                vec![i.clone()].cmp(l)
+            }
+
+            (PacketData::List(l1), PacketData::List(l2)) => l1.cmp(l2),
+            (PacketData::Integer(i1), PacketData::Integer(i2)) => i1.cmp(i2),
+        }
+    }
 }
 
 impl fmt::Display for PacketData {
@@ -87,11 +109,20 @@ fn parse(input: &str) -> Vec<[PacketData; 2]> {
 
 pub fn task1(input: &str) -> SolutionResult {
     let pairs: Vec<[PacketData; 2]> = parse(input);
-    //for pair in pairs {
-    //    println!("{}\n{}\n", pair[0], pair[1]);
-    //}
 
-    SolutionResult::Unsigned(0)
+    let mut res = 0;
+    for (i, pair) in pairs.iter().enumerate() {
+        //println!(
+        //    "The lists are in the {} order",
+        //    if pair[0] < pair[1] { "right" } else { "wrong" }
+        //);
+        //println!("{}\n{}\n", pair[0], pair[1]);
+        if pair[0] < pair[1] {
+            res += i + 1
+        }
+    }
+
+    SolutionResult::Unsigned(res)
 }
 
 pub fn task2(input: &str) -> SolutionResult { SolutionResult::Unsigned(0) }
